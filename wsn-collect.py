52d5e653148f8c79ec0bf38bce7c973d2e33a778
humwerthuz <heineken69addict@gmail.com>
2013-11-10 14:27:03 -0300

    added view to render ajax. Changed index to template

--- wsn-collect.py
+++ wsn-collect.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template
 from wsn_driver.driver import WsnCollectDriver
 
 app = Flask(__name__)
 app_driver = WsnCollectDriver()
 
-@app.route('/')
-def hello_world():
+
+@app.route('/load')
+def load():
     data = app_driver.get_storage().get('key')
     if data:
-        return "Received [%s]" % data
+        return "Received [%s] from [%s]" % (data[0], data[1])
     else:
         return "No data available"
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
